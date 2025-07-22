efa55209a3120ac2e4aedd09df8d89180d644e56
Gaurav Sharma <131950180+gauravsharma5166@users.noreply.github.com>
2025-07-22 23:46:51 +0530

    made ai tools grid and completed hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import { useNavigate } from 'react-router-dom'
+import { useUser } from '@clerk/clerk-react'
 
 const Home = () => {
+
   return (
     <>
     <Navbar />
+    <Hero />
+    <AiTools />
     </>
   )
 }
