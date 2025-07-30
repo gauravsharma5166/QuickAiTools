96aed3905150a965498598534bcb5501b58d5ac6
Gaurav Sharma <131950180+gauravsharma5166@users.noreply.github.com>
2025-07-30 20:56:34 +0530

    setup AI api's and tested fetching for half of ai tools

--- server/server.js
+++ server/server.js
@@ -3,18 +3,21 @@ import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware, requireAuth } from '@clerk/express'
 import aiRouter from './routes/aiRoutes.js';
+import connectCloudinary from './configs/cloudinary.js';
 
-const app=express();
+const app = express();
+
+await connectCloudinary();
 
 app.use(cors());
 app.use(express.json());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => res.send('Server is live!'))
 
 app.use(requireAuth())
 
-app.use('/api/ai', aiRouter)
+app.use('/api/ai', aiRouter);
 
 const PORT = process.env.PORT || 3000;
 
